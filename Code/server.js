d4f72c3816437e51991fee5251fb3282986a3404
Charlie Fish <fishcharlie@me.com>
2016-08-22 08:43:53 -0600

    Adding basic routes

--- Code/server.js
+++ Code/server.js
@@ -27,6 +27,69 @@ dynamoose.setDefaults({
 });
 
 
+// Get
+app.get("/questions", function(req, res) {
+  var questionsArray = [];
+    Question.scan({}, function(err, questions) {
+      if (err) {
+        res.status(500).send('Error');
+      }
+      else {
+        questionsArray = questions;
+        var lastKey = questions.lastKey;
+        while (lastKey) {
+          Question.scan().startAt(questions.lastKey).exec(function (err, questions) {
+            lastKey = questions.lastKey;
+            questions.forEach(function (item) {
+              questionsArray.push(item);
+            });
+          });
+        }
+        res.status(200).send(questionsArray);
+      }
+    });
+});
+
+// Post
+app.post("/questions", function(req, res) {
+    var question = new Question({
+      question: req.body.question,
+      tags: req.body.tags,
+    });
+
+    question.save(function (err) {
+        if (err) {
+            res.status(500).send('Error');
+        }
+        else {
+            res.status(200).send(question);
+        }
+    });
+
+
+});
+
+// // Put
+// app.put("/flashcards/:id", function(req, res) {
+//     Card.findById(req.param('id'), function(err, card) {
+//         if (req.body.question) {
+//             card.question = req.body.question;
+//         }
+//         if (req.body.answer) {
+//             card.answer = req.body.answer;
+//         }
+//         card.save();
+//         res.send('OK');
+//     });
+// });
+//
+// // Delete
+// app.delete("/flashcards/:id", function(req, res) {
+//     Card.findById(req.param('id'), function(err, card) {
+//       card.remove();
+//       res.send('OK');
+//     });
+// });
 
 
 
