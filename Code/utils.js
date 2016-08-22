f6233f3334353beb22b5abdcd1087d58aede5df2
Charlie Fish <fishcharlie@me.com>
2016-08-21 19:00:28 -0600

    Adding utils file to handle creating ids

---
+++ Code/utils.js
@@ -0,0 +1,18 @@
+var crypto = require('crypto');
+
+module.exports.createID = function(id) {
+    return crypto.createHash('sha1').update(id).digest('hex');
+};
+
+
+module.exports.uid = function(len) {
+  var buf = [],
+      chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789',
+      charlen = chars.length;
+
+  for (var i = 0; i < len; ++i) {
+    buf.push(chars[getRandomInt(0, charlen - 1)]);
+  }
+
+  return buf.join('');
+};
